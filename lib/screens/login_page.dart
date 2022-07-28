84f0016ca15cec68cebf59cc648ef63c7cf62f56
Azevedo-Erick <azevedoerick762@gmail.com>
2022-07-28 13:30:59 -0300

    Formulario para cadastro completo

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -8,7 +8,33 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  bool _isLogin = false;
+
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this, // the SingleTickerProviderStateMixin
+      duration: Duration(milliseconds: 500),
+    );
+  }
+
+  @override
+  void didUpdateWidget(LoginPage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _animationController.duration = Duration(milliseconds: 500);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +59,8 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
           Positioned(
-            top: 0,
+            top: _isLogin ? 0 : null,
+            bottom: _isLogin ? null : 0,
             left: 0,
             child: Container(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
@@ -46,103 +73,293 @@ class _LoginPageState extends State<LoginPage> {
                   bottomRight: Radius.circular(30),
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Icon(Icons.monetization_on,
-                          color: Colors.black, size: 72)),
-                  Spacer(),
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 31, 122, 114),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(
-                          color: Color.fromARGB(255, 31, 122, 114),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
+              child: _isLogin
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            height: MediaQuery.of(context).size.height * 0.1,
+                            child: Icon(Icons.monetization_on,
+                                color: Colors.black, size: 72)),
+                        Spacer(),
+                        Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
                             color: Color.fromARGB(255, 31, 122, 114),
                           ),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              labelStyle: TextStyle(
+                                color: Color.fromARGB(255, 31, 122, 114),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                color: Color.fromARGB(255, 31, 122, 114),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () {},
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 31, 122, 114),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Text(
+                              'Login',
+                              style: GoogleFonts.nunito(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text('Dont have an account?'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text('Forgot Password?'),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            height: MediaQuery.of(context).size.height * 0.1,
+                            child: Icon(Icons.monetization_on,
+                                color: Colors.black, size: 72)),
+                        Spacer(),
+                        Text(
+                          'Register',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
                             color: Color.fromARGB(255, 31, 122, 114),
                           ),
                         ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(
-                          color: Color.fromARGB(255, 31, 122, 114),
+                        SizedBox(
+                          height: 20,
                         ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color.fromARGB(255, 31, 122, 114),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              labelStyle: TextStyle(
+                                color: Color.fromARGB(255, 31, 122, 114),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color.fromARGB(255, 31, 122, 114),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                color: Color.fromARGB(255, 31, 122, 114),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Spacer(),
-                  TextButton(
-                    onPressed: () {},
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 31, 122, 114),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Text(
-                        'Login',
-                        style: GoogleFonts.nunito(
-                          fontSize: 20,
-                          color: Colors.white,
+                        SizedBox(
+                          height: 20,
                         ),
-                      ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Confirm Password',
+                              labelStyle: TextStyle(
+                                color: Color.fromARGB(255, 31, 122, 114),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 31, 122, 114),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () {},
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 31, 122, 114),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Text(
+                              'Registrar',
+                              style: GoogleFonts.nunito(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text('Fazer Login')),
+                      ],
                     ),
-                  ),
-                  Spacer(),
-                  TextButton(onPressed: () {}, child: Text('Forgot Password?')),
-                ],
-              ),
             ),
-          ),
+          )
+
+          // ),
         ],
       ),
     );
   }
 }
+
+// class _CustomClipper extends CustomClipper<Path> {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     Paint paint0 = Paint()
+//       ..color = const Color.fromARGB(255, 33, 150, 243)
+//       ..style = PaintingStyle.stroke
+//       ..strokeWidth = 1;
+
+//     Path path0 = Path();
+//     path0.moveTo(size.width * 0.0000130, size.height * 0.0004396);
+//     path0.lineTo(size.width * 0.0010156, size.height * 0.5777080);
+//     path0.quadraticBezierTo(size.width * 0.1704297, size.height * 0.4928414,
+//         size.width * 0.2963542, size.height * 0.5796703);
+//     path0.cubicTo(
+//         size.width * 0.3724219,
+//         size.height * 0.6377708,
+//         size.width * 0.5392187,
+//         size.height * 0.5930769,
+//         size.width * 0.6201693,
+//         size.height * 0.5844584);
+//     path0.quadraticBezierTo(size.width * 0.7261979, size.height * 0.5458870,
+//         size.width * 0.9986979, size.height * 0.5792936);
+//     path0.lineTo(size.width, 0);
+//     path0.lineTo(size.width * 0.0000130, size.height * 0.0004396);
+//     path0.close();
+
+//     canvas.drawPath(path0, paint0);
+//   }
+
+//   @override
+//   Path getClip(Size size) {
+//     Path path0 = Path();
+//     path0.moveTo(0, size.height * 0.0019623);
+//     path0.lineTo(0, size.height * 1.0031083);
+//     path0.quadraticBezierTo(size.width * 0.2227083, size.height * 1.0467504,
+//         size.width * 0.2986589, size.height * 1.0228885);
+//     path0.quadraticBezierTo(size.width * 0.4364974, size.height * 0.9618367,
+//         size.width, size.height * 0.9993407);
+//     path0.lineTo(size.width, 0);
+//     path0.lineTo(0, size.height * 0.0019623);
+//     path0.close();
+//     return path0;
+//   }
+
+//   @override
+//   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+//     return true;
+//   }
+// }
